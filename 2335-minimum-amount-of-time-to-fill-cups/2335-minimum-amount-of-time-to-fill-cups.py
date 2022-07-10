804de5ddee7b255703a12f96b42a39b4fd8d5ab8
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-07-10 07:28:28 +0300

    Time: 38 ms (83.33%), Space: 13.9 MB (16.67%) - LeetHub

---
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
@@ -0,0 +1,18 @@
+import heapq
+
+class Solution:
+    def fillCups(self, amount: List[int]) -> int:
+        amount.sort(reverse=True)
+        print(amount)
+        
+        seconds = 0
+        while amount[0] != 0:
+            if amount[0] > 0:
+                amount[0] -= 1
+            if amount[1] > 0:
+                amount[1] -= 1
+            
+            seconds += 1
+            amount.sort(reverse=True)
+        
+        return seconds
