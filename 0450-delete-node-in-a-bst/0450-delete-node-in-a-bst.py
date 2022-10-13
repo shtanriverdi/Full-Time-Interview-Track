da7a8a604a10257e92a6038588790108099bfd04
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-10-13 17:33:04 +0300

    Time: 144 ms (49.26%), Space: 18.4 MB (27.02%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -5,51 +5,50 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findBiggestNode(self, current_node):
-        walk = current_node
+    def findSuccessor(self, root):
+        walk = root.right
+        if walk == None:
+            return None
+        
+        while walk.left:
+            walk = walk.left
+            
+        return walk
+            
+    def findPredecessor(self, root):
+        walk = root.left
+        if walk == None:
+            return None
+        
         while walk.right:
             walk = walk.right
+            
         return walk
     
-    def searchNodeToBeDeleted(self, current_node, parent, key, came_from):
-        if current_node == None:
-            return [ None, None, 'N' ]
+    def findAndDeleteNodeHelper(self, current, key):
+        if current == None:
+            return None
         
-        if current_node.val == key:
-            return [ parent, current_node, came_from ]
-        
-        if key > current_node.val:
-            return self.searchNodeToBeDeleted(current_node.right, current_node, key, 'R')
-    
-        return self.searchNodeToBeDeleted(current_node.left, current_node, key, 'L')
-        
+        if key < current.val:
+            current.left = self.findAndDeleteNodeHelper(current.left, key)
+        elif key > current.val:
+            current.right = self.findAndDeleteNodeHelper(current.right, key)
+        else:
+            successor = self.findSuccessor(current)
+            predecessor = self.findPredecessor(current) # TODO - Optimize
+            if successor:
+                current.val = successor.val
+                current.right = self.findAndDeleteNodeHelper(current.right, successor.val)
+            elif predecessor:
+                predecessor = self.findPredecessor(current)
+                if predecessor:
+                    current.val = predecessor.val
+                    current.left = self.findAndDeleteNodeHelper(current.left, predecessor.val)
+            else:
+                return None
+                
+        return current
+                
     
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        dummy_root = TreeNode(1453)
-        dummy_root.right = root
-        
-        [ parent, node_to_be_deleted, came_from ] = self.searchNodeToBeDeleted(root, dummy_root, key, 'R')
-        
-        if node_to_be_deleted == None:
-            return dummy_root.right
-        
-        left_subtree_after = node_to_be_deleted.left
-        right_subtree_after = node_to_be_deleted.right
-        
-        if left_subtree_after:
-            if came_from == 'R':
-                parent.right = left_subtree_after
-            else:
-                parent.left = left_subtree_after
-                
-            node_at_lower_right_corner = self.findBiggestNode(left_subtree_after)
-            if right_subtree_after:
-                node_at_lower_right_corner.right = right_subtree_after
-                
-        else:
-            if came_from == 'R':
-                parent.right = right_subtree_after
-            else:
-                parent.left = right_subtree_after
-            
-        return dummy_root.right
+        return self.findAndDeleteNodeHelper(root, key)
