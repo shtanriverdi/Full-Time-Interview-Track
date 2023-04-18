481cb7b54a1b1b043894a62c09edeca2236d63ec
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2023-04-18 16:46:33 +0300

    Time: 37 ms (22.14%), Space: 13.7 MB (95.97%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -4,15 +4,13 @@ class Solution:
         
         ptrA, lengthA = 0, len(word1)
         ptrB, lengthB = 0, len(word2)
-        turn = True
         
         while ptrA < lengthA or ptrB < lengthB:
-            if ptrA < lengthA and turn:
+            if ptrA < lengthA:
                 merged.append(word1[ptrA])
                 ptrA += 1
-            if ptrB < lengthB and not turn:
+            if ptrB < lengthB:
                 merged.append(word2[ptrB])
                 ptrB += 1
-            turn = not turn
             
         return "".join(merged)
